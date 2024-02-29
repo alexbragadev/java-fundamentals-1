3f92eed6e9f23d6a442c480346a5ff13048854bf
C1333355 Alex Araujo De Braga <c1333355@interno.bb.com.br>
2024-02-29 17:37:19 -0300

    alterações para correção de exercício

--- src/interfaces/application/Program.java
+++ src/interfaces/application/Program.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 
 import interfaces.model.entities.CarRental;
 import interfaces.model.entities.Vehicle;
+import interfaces.model.services.BrazilTaxService;
+import interfaces.model.services.RentalService;
 
 public class Program {
 
@@ -25,7 +27,21 @@ public class Program {
 		System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
 		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 		
-		CarRental car = new CarRental(start, finish, new Vehicle(carModel));
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		
+		System.out.print("Entre com o preço por hora: ");
+		double pricePerHour = sc.nextDouble();
+		System.out.print("Entre com o preço por dia: ");
+		double pricePerDay = sc.nextDouble();
+		
+		RentalService rentalService = new RentalService(pricePerDay, pricePerHour, new BrazilTaxService());
+		
+		rentalService.processInvoice(cr);
+
+		System.out.println("FATURA:");
+		System.out.println("Pagamento basico: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+		System.out.println("Imposto: " + String.format("%.2f", cr.getInvoice().getTax()));
+		System.out.println("Pagamento total: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
 		
 		sc.close();
 
