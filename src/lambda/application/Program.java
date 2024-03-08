afba89d4d8ad2ba196864362e2bff6ce26d00a2a
C1333355 Alex Araujo De Braga <c1333355@interno.bb.com.br>
2024-03-08 16:52:19 -0300

    map and stream

--- src/lambda/application/Program.java
+++ src/lambda/application/Program.java
@@ -1,13 +1,18 @@
 package lambda.application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import lambda.entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
 		
 		list.add(new Product("Smart Tv Lg '55'", 3.500));
@@ -22,6 +27,21 @@ public class Program {
 		for (Product p : list) {
 			System.out.println(p);
 		}
+		
+		// stream é uma sequecia de elementos advinda de alguma fonte
+		// uso da função map aplica uma nova função a cada elemento da stream
+		List<String> names = list.stream()
+									.map(p -> p.getName().toLowerCase())
+										.collect(Collectors.toList());
+
+		names.forEach(System.out::println);
+		
+		// streams
+		List<Integer> listN = Arrays.asList(3,4,5,10,7);
+		
+		Stream<Integer> st1 = listN.stream().map(x -> x * 10);
+		System.out.println(Arrays.toString(st1.toArray()));
+		
 	}
 
 }
